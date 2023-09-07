7a032b6003e92813007053ffb1f1aa3f97aaf0e7
MCharmoille <100427930+MCharmoille@users.noreply.github.com>
2023-09-07 08:57:16 -0400

    Chemin vers node corrigé

--- client/truckmaster/src/pages/Commandes.jsx
+++ client/truckmaster/src/pages/Commandes.jsx
@@ -7,7 +7,7 @@ const Commandes = () => {
   useEffect(() => {
     const fetchAllCommandes = async () => {
       try {
-        const res = await axios.get('http://localhost:8800/commandes');
+        const res = await axios.get('http://37.187.55.12:8800/commandes');
         setCommandes(res.data);
       } catch (err) {
         console.log(err);
