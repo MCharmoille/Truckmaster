5448d499e78ec93d535d9aad4b05b48cea523732
MCharmoille <100427930+MCharmoille@users.noreply.github.com>
2023-09-27 19:07:40 +0200

    standardisation

--- client/truckmaster/src/components/NouvelleCommande.jsx
+++ client/truckmaster/src/components/NouvelleCommande.jsx
@@ -2,14 +2,14 @@ import axios from 'axios';
 import React from 'react';
 import { useState, useEffect } from 'react';
 import { useNavigate, useParams } from 'react-router-dom';
-import Modal from "./modals/CustomProduit";
+import ModalCustom from "./modals/CustomProduit";
 import ModalBoissons from "./modals/Boissons";
 import Confirm from "./modals/Confirm";
 import moment from 'moment';
 
 const Add = () => {
     const { commandeId } = useParams();
-    const [commande, set_commande] = useState({
+    const [commande, setCommande] = useState({
         libelle:"",
         date:moment(new Date()).format("YYYY-MM-DD"),
         time:"",
@@ -17,16 +17,16 @@ const Add = () => {
     });
     // TO DO : remplacer par une requête à l'API
     const produits_affiches = [
-        {id : 1, display : true, onclick : () => modifier_commande(1, 1), nom : "Campagnard", prix : 10},
-        {id : 2, display : true, onclick : () => modifier_commande(2, 1), nom : "Pouly", prix : 10},
-        {id : 5, display : true, onclick : () => modifier_commande(5, 1), nom : "Classique", prix : 9},
-        {id : 6, display : true, onclick : () => modifier_commande(6, 1), nom : "Lo'cale", prix : 11},
-        {id : 7, display : true, onclick : () => modifier_commande(7, 1), nom : "Végé", prix : 9},
-        {id : 8, display : true, onclick : () => modifier_commande(8, 1), nom : "Menu enfant", prix : 7},
-        {id : 9, display : true, onclick : () => modifier_commande(9, 1), nom : "Spécial mois", prix : 11},
-        {id : 99, display : true, onclick : () => modifier_commande(99, 1),  nom : "Autre", prix : 0},
-        {id : 3, display : true, onclick : () => modifier_commande(3, 1), nom : "Frite", prix : 3},
-        {id : 98, display : true, onclick : () => handleClickBoissons(),  nom : "Boisson", prix : 2},
+        {id : 1, display : true, onclick : () => modifierCommande(1, 1), nom : "Campagnard", prix : 10},
+        {id : 2, display : true, onclick : () => modifierCommande(2, 1), nom : "Pouly", prix : 10},
+        {id : 5, display : true, onclick : () => modifierCommande(5, 1), nom : "Classique", prix : 9},
+        {id : 6, display : true, onclick : () => modifierCommande(6, 1), nom : "Lo'cale", prix : 11},
+        {id : 7, display : true, onclick : () => modifierCommande(7, 1), nom : "Végé", prix : 9},
+        {id : 8, display : true, onclick : () => modifierCommande(8, 1), nom : "Menu enfant", prix : 7},
+        {id : 9, display : true, onclick : () => modifierCommande(9, 1), nom : "Spécial mois", prix : 11},
+        {id : 99, display : true, onclick : () => modifierCommande(99, 1),  nom : "Autre", prix : 0},
+        {id : 3, display : true, onclick : () => modifierCommande(3, 1), nom : "Frite", prix : 3},
+        {id : 98, display : true, onclick : () => setModalBoissons(true),  nom : "Boisson", prix : 2},
         {id : 10, display : false, nom : "Coca", prix : 2},
         {id : 11, display : false, nom : "Orangina", prix : 2},
         {id : 12, display : false, nom : "Schweppes", prix : 2},
@@ -35,23 +35,23 @@ const Add = () => {
         {id : 15, display : false, nom : "Bière", prix : 2},
         {id : 16, display : false, nom : "Bière locale", prix : 3.5}
     ];
-    const [produits_commandes, set_produits_commandes] = useState([]);
-    const [temp_id, set_temp_id] = useState(0);
-    const [total, set_total] = useState(0);
+    const [produitsCommandes, setProduitsCommandes] = useState([]);
+    const [tempId, setTempId] = useState(0);
+    const [total, setTotal] = useState(0);
     const navigate = useNavigate();
 
     useEffect(() => {
         if (commandeId) {
             axios.get(process.env.REACT_APP_API_URL+`commandes/id/${commandeId}`)
                 .then((response) => {
-                    set_commande(({
+                    setCommande(({
                         libelle: response.data.libelle,
                         date: moment(response.data.date_commande).format('YYYY-MM-DD'),
                         time: moment(response.data.date_commande).format('HH:mm'),
                         paye: response.data.moyen_paiement
                     }));
-                    set_produits_commandes(response.data.produits);
-                    set_temp_id(response.data.produits.length);
+                    setProduitsCommandes(response.data.produits);
+                    setTempId(response.data.produits.length);
                 })
                 .catch((error) => {
                     console.error("Erreur lors de la récupération de la commande existante :", error);
@@ -64,10 +64,10 @@ const Add = () => {
         // TO DO : aller chercher les stocks pour la date sélectionné
     }, [commande.date]);
 
-    const handleClick = async () => {
+    const validerCommande = async () => {
         const updatedCommande = {  libelle: document.querySelector('#input_libelle').value, 
                                    date_commande: `${document.querySelector('#input_date').value} ${document.querySelector('#input_time').value}:00`, 
-                                   produits: produits_commandes
+                                   produits: produitsCommandes
                                 };
         try {
             if (commandeId) await axios.post(process.env.REACT_APP_API_URL+"commandes/update/"+commandeId, updatedCommande);
@@ -78,63 +78,54 @@ const Add = () => {
         }
     };
 
-    const modifier_commande = (id, qte) => {
+    const modifierCommande = (id, qte) => {
         var id_pc = -1;
         
-        if(qte === -1){ // suppression d'un item, on cherche l'temp_id de la cible
-            id_pc = produits_commandes.findIndex((p) => (p.temp_id === id));
+        if(qte === -1){ // suppression d'un item, on cherche l'tempId de la cible
+            id_pc = produitsCommandes.findIndex((p) => (p.tempId === id));
         }
         else { // ajout d'un item, on utilise l'id produit
-            id_pc = produits_commandes.findIndex((p) => (p.id_produit === id) && (!p.modifications || p.modifications.length === 0));
+            id_pc = produitsCommandes.findIndex((p) => (p.id_produit === id) && (!p.modifications || p.modifications.length === 0));
         }
 
         if (id_pc !== -1) { // item trouvé dans la liste
-            const pc_clone = [...produits_commandes];
+            const pc_clone = [...produitsCommandes];
             pc_clone[id_pc].qte += qte;
             if(pc_clone[id_pc].qte < 1) pc_clone.splice(id_pc, 1);
-            set_produits_commandes(pc_clone);
+            setProduitsCommandes(pc_clone);
         } else { // nouvel item
             var produit = produits_affiches[produits_affiches.findIndex((p) => p.id === id)];
-            set_produits_commandes([...produits_commandes, { id_produit: produit.id, nom: produit.nom, prix: produit.prix, qte: 1, temp_id: temp_id}]);
-            set_temp_id(temp_id + 1);
+            setProduitsCommandes([...produitsCommandes, { id_produit: produit.id, nom: produit.nom, prix: produit.prix, qte: 1, tempId: tempId}]);
+            setTempId(tempId + 1);
         }
     };
 
     useEffect(() => {
         var new_total = 0;
         
-        for(var i in produits_commandes){
-            new_total += produits_commandes[i].qte * produits_commandes[i].prix;
+        for(var i in produitsCommandes){
+            new_total += produitsCommandes[i].qte * produitsCommandes[i].prix;
         }
-        set_total(new_total);
-    }, [produits_commandes]);
+        setTotal(new_total);
+    }, [produitsCommandes]);
 
-    const [showModal, setShowModal] = useState(false);
+    const [showModalCustom, setModalCustom] = useState(false);
     const [selectedProduct, setSelectedProduct] = useState(null);
 
-    const handleClickProduit = (produit) => {
-        setSelectedProduct(produit);
-        setShowModal(true);
-    };
-
-    const handleCloseModal = (modifications) => {
-        setShowModal(false);
+    const CloseModalCustom = (modifications) => {
+        setModalCustom(false);
         
-        const id_pc = produits_commandes.findIndex((p) => p.temp_id === selectedProduct.temp_id);
-        const pc_clone = [...produits_commandes];
+        const id_pc = produitsCommandes.findIndex((p) => p.tempId === selectedProduct.tempId);
+        const pc_clone = [...produitsCommandes];
         pc_clone[id_pc].modifications = modifications;
-        set_produits_commandes(pc_clone);
+        setProduitsCommandes(pc_clone);
     };
 
-    const [showModalBoissons, setShowModalBoissons] = useState(false);
+    const [showModalBoissons, setModalBoissons] = useState(false);
 
-    const handleClickBoissons = () => {
-        setShowModalBoissons(true);
-    };
-
-    const handleCloseModalBoissons = (id) => {
-        setShowModalBoissons(false);
-        modifier_commande(id, 1);
+    const CloseModalBoisson = (id) => {
+        setModalBoissons(false);
+        modifierCommande(id, 1);
     };
 
     const [showConfirm, setshowConfirm] = useState(false);
@@ -162,8 +153,8 @@ const Add = () => {
                 <div className='recap'> 
                     <input type='text' id='input_libelle' placeholder='Nouvelle Commande' name='libelle' defaultValue={commande.libelle}/>
                     <div> 
-                        <input id="input_date" type="date" value={commande.date} onChange={(e) => {set_commande((prevCommande) => ({ ...prevCommande, date: e.target.value }));}}/> 
-                        <select id='input_time' name='time' value={commande.time} onChange={(e) => {set_commande((prevCommande) => ({ ...prevCommande, time: e.target.value }));}}>
+                        <input id="input_date" type="date" value={commande.date} onChange={(e) => {setCommande((prevCommande) => ({ ...prevCommande, date: e.target.value }));}}/> 
+                        <select id='input_time' name='time' value={commande.time} onChange={(e) => {setCommande((prevCommande) => ({ ...prevCommande, time: e.target.value }));}}>
                             <option> 18:00 </option><option> 18:15 </option><option> 18:30 </option><option> 18:45 </option>
                             <option> 19:00 </option><option> 19:15 </option><option> 19:30 </option><option> 19:45 </option>
                             <option> 20:00 </option><option> 20:15 </option><option> 20:30 </option><option> 20:45 </option>
@@ -174,12 +165,12 @@ const Add = () => {
                     <hr/>
 
                     <div className='contenu_commande'>
-                    {produits_commandes.map((produit, index) => (
+                    {produitsCommandes.map((produit, index) => (
                         <div key={index}> 
                             <div style={{ display: 'flex', justifyContent: 'space-between' }}> 
                                 <div> 
-                                    <span className='enlever_article' onClick={() => modifier_commande(produit.temp_id, -1)}> - </span> 
-                                    <span onClick={() => handleClickProduit(produit)}>
+                                    <span className='enlever_article' onClick={() => modifierCommande(produit.tempId, -1)}> - </span> 
+                                    <span onClick={() => {setSelectedProduct(produit); setModalCustom(true);}}>
                                         {produit.qte} x {produit.nom}
                                     </span>
                                 </div> 
@@ -202,20 +193,19 @@ const Add = () => {
                 <div className='deja_paye'> { commande.paye !== null ? "Attention, cette commande à déjà été payée." :""} </div>
                 {commandeId ? 
                     <div className='button-container'>
-                        <div className='ajouter_small' onClick={handleClick}> Modifier </div>
+                        <div className='ajouter_small' onClick={validerCommande}> Modifier </div>
                         <div className='suppr_small' onClick={() => setshowConfirm(true)}> X </div>
                     </div>
                             : 
-                    <div className='ajouter' onClick={handleClick}> Ajouter </div>
+                    <div className='ajouter' onClick={validerCommande}> Ajouter </div>
                 }
             </div>
 
-            {/* Afficher la fenêtre modale si showModal est true */}
-            {showModal && (
-                <Modal produit={selectedProduct} onClose={handleCloseModal} />
+            {showModalCustom && (
+                <ModalCustom produit={selectedProduct} onClose={CloseModalCustom} />
             )}
             {showModalBoissons && (
-                <ModalBoissons onClose={handleCloseModalBoissons} />
+                <ModalBoissons onClose={CloseModalBoisson} />
             )}
             {showConfirm && (
                 <Confirm message="Voulez vous vraiment supprimer cette commande ?" onClose={supprimerCommande} />
